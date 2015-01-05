9de07fb4c543e4d7b847f6dd36b10c8956c13a86
Alexander Ulrich <alex@etc-network.de>
2015-01-05 15:48:20 +0100

    Pretty-printing and pairs.

--- Internals3.hs
+++ Internals3.hs
@@ -12,37 +12,38 @@
 import           Control.Applicative
 import           Control.Monad
 import           Control.Monad.ConstrainedNormal
+import           Control.Monad.State
+
 
 import           Data.Text                       (Text)
-import           Prelude                         hiding (concatMap)
-import           Text.PrettyPrint.ANSI.Leijen
+import           Text.PrettyPrint.ANSI.Leijen    hiding ((<$>))
+import           Text.Printf
 
 --------------------------------------------------------------------------------
 -- Typed query AST
 
 data Exp :: * -> * where
-  BoolE       :: Bool    -> Exp QBool
-  IntegerE    :: Integer -> Exp QInt
-  ListE       :: [Exp a] -> Exp (QList a)
+    BoolE       :: Bool    -> Exp QBool
+    IntegerE    :: Integer -> Exp QInt
+    ListE       :: [Exp a] -> Exp (QList a)
+    TupE        :: Exp a -> Exp b -> Exp (QTup a b)
 
-  AndE        :: Exp (QList QBool) -> Exp QBool
-  SngE        :: Exp a -> Exp (QList a)
-  AppendE     :: Exp (QList a) -> Exp (QList a) -> Exp (QList a)
-  ConcatMapE  :: Exp (a -> QList b) -> Exp (QList a) -> Exp (QList b)
-  LamE        :: (Reify a, Reify b)  => (Exp a -> Exp b) -> Exp (a -> b)
-  VarE        :: (Reify a)           => Integer -> Exp a
+    AndE        :: Exp (QList QBool) -> Exp QBool
+    SngE        :: Exp a -> Exp (QList a)
+    AppendE     :: Exp (QList a) -> Exp (QList a) -> Exp (QList a)
+    ConcatMapE  :: Exp (a -> QList b) -> Exp (QList a) -> Exp (QList b)
+    LamE        :: (Reify a, Reify b)  => (Exp a -> Exp b) -> Exp (a -> b)
+    VarE        :: (Reify a)           => Integer -> Exp a
+    FstE        :: Exp (QTup a b) -> Exp a
+    SndE        :: Exp (QTup a b) -> Exp b
+    TableE      :: String -> Exp (QList (QTup a b))
 
 data Type :: * -> * where
-  BoolT     :: Type QBool
-  IntegerT  :: Type QInt
-  ListT     :: (Reify a)          => Type a -> Type (QList a)
-  ArrowT    :: (Reify a,Reify b)  => Type a -> Type b -> Type (a -> b)
-
-instance Pretty (Type a) where
-    pretty BoolT          = text "Bool"
-    pretty IntegerT       = text "Integer"
-    pretty (ListT t)      = brackets $ pretty t
-    pretty (ArrowT t1 t2) = parens $ pretty t1 <+> text "->" <+> pretty t2
+    BoolT     :: Type QBool
+    IntegerT  :: Type QInt
+    ListT     :: (Reify a)          => Type a -> Type (QList a)
+    TupT      :: Type a -> Type b -> Type (QTup a b)
+    ArrowT    :: (Reify a, Reify b)  => Type a -> Type b -> Type (a -> b)
 
 -- | A combination of column names that form a candidate key
 newtype Key = Key [String] deriving (Eq, Ord, Show)
@@ -94,9 +95,18 @@ instance (Reify a) => Reify (QList a) where
 instance (Reify a, Reify b) => Reify (a -> b) where
     reify _ = ArrowT (reify (undefined :: a)) (reify (undefined :: b))
 
+instance (Reify a, Reify b) => Reify (QTup a b) where
+    reify _ = TupT (reify (undefined :: a)) (reify (undefined :: b))
+
 newtype QInt      = QInt (Exp (Rep QInt))
 newtype QBool     = QBool (Exp (Rep QBool))
 newtype QList a   = QList (Exp (Rep (QList a)))
+newtype QTup a b  = QTup (Exp (Rep (QTup a b)))
+
+instance (Q a, Q b) => Q (QTup a b) where
+    type Rep (QTup a b) = QTup (Rep a) (Rep b)
+    wrap = QTup
+    unwrap (QTup e) = e
 
 instance Q QInt where
     type Rep QInt = QInt
@@ -151,25 +161,54 @@ appendRep as bs = wrap $ AppendE (unwrap as) (unwrap bs)
 toLam :: (Q a, Q b) => (a -> b) -> Exp (Rep a) -> Exp (Rep b)
 toLam f = unwrap . f . wrap
 
+emptyRep :: Q a => QList a
+emptyRep = wrap $ ListE []
+
 --------------------------------------------------------------------------------
 -- User-facing list combinators on monadic lists
 
 type QListM a = NM Q QList a
 
-sng :: (Q a, Q (Rep a)) => a -> QListM a
-sng = liftList . sngRep
+fst_ :: (Q a, Q b) => QTup a b -> a
+fst_ t = wrap $ FstE $ unwrap t
 
-and :: QListM QBool -> QBool
-and = andRep . lowerList
+snd_ :: (Q a, Q b) => QTup a b -> b
+snd_ t = wrap $ SndE $ unwrap t
 
-append :: (Q a, Q (Rep a)) => QListM a -> QListM a -> QListM a
-append as1 as2 = liftList $ appendRep (lowerList as1) (lowerList as2)
+sng_ :: (Q a, Q (Rep a)) => a -> QListM a
+sng_ = liftList . sngRep
 
-concatMap :: (Q a, Q (Rep a), Q b, Q (Rep b))
-          => (a -> QListM b) -> QListM a -> QListM b
-concatMap f as = liftList $ concatMapRep f' (lowerList as)
+empty_ :: (Q a, Q (Rep a)) => QListM a
+empty_ = liftList emptyRep
+
+and_ :: QListM QBool -> QBool
+and_ = andRep . lowerList
+
+append_ :: (Q a, Q (Rep a)) => QListM a -> QListM a -> QListM a
+append_ as1 as2 = liftList $ appendRep (lowerList as1) (lowerList as2)
+
+concatMap_ :: (Q a, Q (Rep a), Q b, Q (Rep b))
+           => (a -> QListM b) -> QListM a -> QListM b
+concatMap_ f as = liftList $ concatMapRep f' (lowerList as)
   where
     f' a = lowerList $ f a
+
+map_ :: (Q a, Q (Rep a), Q b, Q (Rep b)) => (a -> b) -> QListM a -> QListM b
+map_ f = concatMap_ (sng_ . f)
+
+table_ :: (Q a, Q b, Q (Rep a), Q (Rep b)) => String -> QListM (QTup a b)
+table_ tabName = wrap $ TableE tabName
+
+{-
+instance MonadPlus (NM Q QList) where
+    mzero = empty_
+    mplus = append_
+-}
+
+as :: QListM (QTup QInt QBool)
+as = table_ "a"
+
+
 
 --------------------------------------------------------------------------------
 -- Literal values in queries
@@ -199,7 +238,7 @@ class QLit a where
 instance QLit Bool where
     type Lit Bool = Rep QBool
     litQ :: Bool -> Lit Bool
-    litQ = QBool . BoolE 
+    litQ = QBool . BoolE
 
 instance QLit Integer where
     type Lit Integer = Rep QInt
@@ -212,5 +251,58 @@ instance QLit a => QLit [a] where
     litQ xs = wrap $ ListE $ map litQ xs
 
 -}
-    
+
 -- [a] -> QListM (
+
+--------------------------------------------------------------------------------
+-- Pretty-printing of frontend ASTs
+
+instance Pretty (Type a) where
+    pretty BoolT          = text "Bool"
+    pretty IntegerT       = text "Integer"
+    pretty (ListT t)      = brackets $ pretty t
+    pretty (ArrowT t1 t2) = parens $ pretty t1 <+> text "->" <+> pretty t2
+
+ppApp1 :: String -> Exp a -> State Integer Doc
+ppApp1 n e = (<+>) (text n) <$> (parens <$> pp e)
+
+ppApp2 :: String -> Exp a -> Exp b -> State Integer Doc
+ppApp2 n e1 e2 = do
+    pe1 <- pp e1
+    pe2 <- pp e2
+    return $ text n <+> parens pe1 <+> parens pe2
+
+pp :: Exp a -> State Integer Doc
+pp (BoolE b) = return $ bool b
+pp (IntegerE i) = return $ integer i
+pp (ListE l) = list <$> mapM pp l
+pp (TupE a b) = tupled <$> (pp a >>= \pa -> pp b >>= \pb -> return [pa, pb])
+pp (AndE bs) = ppApp1 "and" bs
+pp (FstE bs) = ppApp1 "fst" bs
+pp (SndE bs) = ppApp1 "snd" bs
+pp (SngE a) = ppApp1 "sng" a
+pp (AppendE as1 as2) = ppApp2 "append" as1 as2
+pp (ConcatMapE f as) = ppApp2 "concatMap" f as
+pp (VarE i) = return $ text $ "v" ++ show i
+pp (LamE f) = do
+    i <- get
+    put $ i + 1
+    let v = "v" ++ show i
+    pf <- pp (f (VarE i))
+    return $ text (printf "\\%s ->" v) <+> pf
+pp (TableE n) = return $ text $ printf "table(%s)" n
+
+instance Pretty QInt where
+    pretty (QInt e) = evalState (pp e) 0
+
+instance Pretty QBool where
+    pretty (QBool e) = evalState (pp e) 0
+
+instance Pretty (QTup a b) where
+    pretty (QTup e) = evalState (pp e) 0
+
+instance Pretty (QList a) where
+    pretty (QList e) = evalState (pp e) 0
+
+instance (Q a, Q (Rep a)) => Pretty (QListM a) where
+    pretty l = pretty $ lowerList l
